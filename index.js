63c62e84ac0898c890917a434b9e8f0c846c7050
netisdark <94221891+netisdark@users.noreply.github.com>
2024-07-21 16:32:45 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-var http = require('http');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+const nodemailer = require("nodemailer");
+async function main() {
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com", 
+    port: 465, 
+    secure: true, 
+    
+    auth: {
+      user: "pergap3@gmail.com",
+      pass: "pcwakyyzwgalidfs", 
+
+    },
+  });
   
-  res.write("hello");
-  res.end();
-}).listen(8090);
+
+  let info = await transporter.sendMail({
+    from: '"Pergap3" <pergap3@gmail.com>',
+    to: "raisha67789@gmail.com",
+    subject: "Testing, testing, 123",
+    html: `
+    <h1>Request</h1>
+    <p>Someone Wants To Burrow</p>
+    `,
+  });
+
+  console.log(info.messageId); // Random ID generated after successful send (optional)
+}
+
+main()
+.catch(err => console.log(err));
